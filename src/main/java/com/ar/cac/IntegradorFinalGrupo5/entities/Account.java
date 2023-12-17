f7be00a521cfe4a8de477af1847d96bda377a569
luisc2194 <luiscastro.2194@gmail.com>
2023-12-17 13:03:19 -0300

    chore: prueba

---
+++ src/main/java/com/ar/cac/IntegradorFinalGrupo5/entities/Account.java
@@ -0,0 +1,10 @@
+package com.ar.cac.IntegradorFinalGrupo5.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Account {
+
+
+}
---
+++ src/main/java/com/ar/cac/IntegradorFinalGrupo5/repositories/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.ar.cac.IntegradorFinalGrupo5.repositories;
+
+import com.ar.cac.IntegradorFinalGrupo5.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long>{
+
+}
